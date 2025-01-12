3de71389a6475f6c546030884eb47cf12d3fd508
Shyam-Gopal-Shrestha <shyamshrestha1003@gmail.com>
2025-01-13 00:39:13 +1100

    array update

--- datamanipulation.js
+++ datamanipulation.js
@@ -4,12 +4,14 @@
 const fruits = ["Apple", "Banana", "Mango"];
 document.getElementById("result").innerHTML = fruits;
 
+// To push in array
 document.getElementById("push").onclick = function () {
   let itemx = document.getElementById("x").value;
   fruits.push(itemx);
   document.getElementById("result").innerHTML = fruits;
 };
 
+// To pop from array
 document.getElementById("pop").onclick = function () {
   fruits.pop();
   document.getElementById("result").innerHTML = fruits;
