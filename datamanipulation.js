d9ef93860f8331f0779e0cddedc160a1dfefeef6
Shyam Gopal Shrestha <shyamshrestha@Shyams-MBP.gateway>
2025-01-13 00:09:30 +1100

    array manipulation

--- datamanipulation.js
+++ datamanipulation.js
@@ -1,6 +1,16 @@
 // Array manipulation
+
+// console.log(itemx);
 const fruits = ["Apple", "Banana", "Mango"];
-
-const Fruits = fruits.push("Kiwi");
-
 document.getElementById("result").innerHTML = fruits;
+
+document.getElementById("push").onclick = function () {
+  let itemx = document.getElementById("x").value;
+  fruits.push(itemx);
+  document.getElementById("result").innerHTML = fruits;
+};
+
+document.getElementById("pop").onclick = function () {
+  fruits.pop();
+  document.getElementById("result").innerHTML = fruits;
+};
